e899bb7ada19cea53526fe662afb38eeaae93d17
mouradKadraoui <mkadraoui46@gmail.com>
2024-05-17 12:28:02 +0100

    mmmm

--- src/app/condidat/dash-condidat/dash-condidat.component.ts
+++ src/app/condidat/dash-condidat/dash-condidat.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Postulation } from '../../models/postulation.model';
 import { Offer } from '../../models/offer.model';
 import { Candidate } from '../../models/candidate.model';
-=======
+
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
->>>>>>> a04283a323bb28f4b6e890027a585f6af7e2ef42
+
 
 @Component({
   selector: 'app-dash-condidat',
@@ -14,11 +13,10 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './dash-condidat.component.css'
 })
 export class DashCondidatComponent {
-<<<<<<< HEAD
 
   postulations : Postulation[]=[]
 
-  constructor(){}
+  constructor(private route : Router, private authService : AuthService){}
 
   postuler(offer : Offer,candidate :Candidate){
     const newPostulation : Postulation = {
@@ -32,11 +30,11 @@ export class DashCondidatComponent {
 
   }
 
-=======
-constructor(private route : Router, private authService : AuthService){}
+
+
 signOut(){
   this.authService.clearLocalStorage()
   this.route.navigateByUrl("/sign-in")
 }
->>>>>>> a04283a323bb28f4b6e890027a585f6af7e2ef42
+
 }
